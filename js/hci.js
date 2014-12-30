720bd358c70be0ef32059aaf65242d05fed1b323
Michael Bernstein <msb@cs.stanford.edu>
2014-12-30 11:43:25 -0800

    debug mode
    Former-commit-id: 27ff77554f872a4314f01fb4f9a997d45fcb17e8
    Former-commit-id: f907617b37cdcd29559a748b4b3bab008dbe8745
    Former-commit-id: 17e5a2ff84542ae6bbda657973075d41fce48c56

--- js/hci.js
+++ js/hci.js
@@ -12,6 +12,10 @@ $(document).ready(function() {
         var $spy = $(this).scrollspy('refresh')
     });
 
+    if (urlParams['debug'] != "") {
+        $('small.notes').css("display", "block");
+    }
+
 });
 
 function initializeSideNav() {
@@ -51,3 +55,18 @@ function initializeSideNav() {
         $('#sidenav-content').append('<li>' + elem + '</li>');
     });
 }
+
+// from StackOverflow:
+// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
